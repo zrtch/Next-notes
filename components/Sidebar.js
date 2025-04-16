dc110bd0d591ed51265785524394465243ad9efd
zhourentian <15970334351@163.com>
2025-04-16 14:08:42 +0800

    feat: 上传文件

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,6 +4,7 @@ import SidebarSearchField from '@/components/SidebarSearchField';
 import SidebarNoteList from '@/components/SidebarNoteList';
 import EditButton from '@/components/EditButton';
 import NoteListSkeleton from '@/components/NoteListSkeleton';
+import SidebarImport from '@/components/SidebarImport';
 
 // // 移除数据请求部分，为 SidebarNoteList 添加 Suspense 以及 fallback UI NoteListSkeleton
 export default async function Sidebar() {
@@ -33,6 +34,7 @@ export default async function Sidebar() {
             <SidebarNoteList />
           </Suspense>
         </nav>
+        <SidebarImport />
       </section>
     </>
   )
