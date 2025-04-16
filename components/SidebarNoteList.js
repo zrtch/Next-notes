092095b5484e9807dfeb582c2602bb90187d8141
zhourentian <15970334351@163.com>
2025-04-16 10:43:26 +0800

    update：笔记搜索

--- components/SidebarNoteList.js
+++ components/SidebarNoteList.js
@@ -1,24 +1,24 @@
+import SidebarNoteListFilter from '@/components/SidebarNoteListFilter';
 import SidebarNoteItem from '@/components/SidebarNoteItem';
 import { getAllNotes } from '@/lib/redis';
+import { sleep } from '@/lib/utils';
 
 export default async function NoteList() {
-  const sleep = ms => new Promise(r => setTimeout(r, ms))
-  // await sleep(3000)
+
+  await sleep(1000)
   const notes = await getAllNotes()
 
-  const arr = Object.entries(notes);
-
-  if (arr.length == 0) {
+  if (Object.entries(notes).length == 0) {
     return <div className="notes-empty">
       {'No notes created yet!'}
     </div>
   }
 
-  return <ul className="notes-list">
-    {arr.map(([noteId, note]) => {
-      return <li key={noteId}>
-        <SidebarNoteItem noteId={noteId} note={JSON.parse(note)} />
-      </li>
-    })}
-  </ul>
+  return (
+    <SidebarNoteListFilter>
+      {Object.entries(notes).map(([noteId, note]) => {
+        return <SidebarNoteItem key={noteId} noteId={noteId} note={JSON.parse(note)} />
+      })}
+    </SidebarNoteListFilter>
+  )
 }
